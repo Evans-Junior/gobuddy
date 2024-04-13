e8309eaa624d1321a6702f5d3b4feb2b0287dd16
Evans kumi <kwakukumi14@gmail.com>
2024-04-13 04:40:10 +0000

    databade notificatiom

---
+++ functions/fetch_trip_requests_notification.php
@@ -0,0 +1,46 @@
+<?php
+// Include database connection code (assuming you have a db_connect.php file)
+include '../settings/connection.php';
+
+$sql = "SELECT 
+TripRequests.RequestID, 
+TripRequests.TimeCreated, 
+TripRequests.Status,
+Trips.Destination, 
+Trips.Location, 
+Trips.Date, 
+Trips.Start, 
+RequesterUser.Username AS RequesterName,
+TripOwnerUser.Username AS TripOwnerName,
+TripRequests.RequesterUserID,
+Trips.UserID AS TripOwnerID
+FROM 
+TripRequests
+JOIN 
+Trips ON TripRequests.TripID = Trips.TripID
+JOIN 
+Users AS RequesterUser ON TripRequests.RequesterUserID = RequesterUser.UserID
+JOIN 
+Users AS TripOwnerUser ON Trips.UserID = TripOwnerUser.UserID
+WHERE 
+TripRequests.TimeCreated < NOW() 
+AND (Trips.TripStatus = 'Active' and TripRequests.Status = 'Pending' or TripRequests.Status = 'Rejected' or TripRequests.Status = 'Accepted' )
+ORDER BY 
+TripRequests.TimeCreated DESC;" ;
+
+$result = mysqli_query($con, $sql);
+
+$tripRequests = array();
+while ($row = mysqli_fetch_assoc($result)) { 
+    $tripRequests[] = $row;
+}
+
+// Close database connection
+mysqli_close($con);
+
+
+
+// Output trip request data as JSON
+header('Content-Type: application/json');
+echo json_encode($tripRequests);
+?>
